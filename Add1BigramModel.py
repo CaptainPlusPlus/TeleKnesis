44ad80a5ab64cdd916e8892bf32399443c47bb73
Felix Redfox <felix.fjellreven@gmail.com>
2021-12-18 21:39:28 +0100

    initial generate sentence code

--- Add1BigramModel.py
+++ Add1BigramModel.py
@@ -25,7 +25,16 @@ UNKNOWN = "<UNK>"
 #  The returned sentence is a list of words ['<s>', 'w1', ..., 'wn', '</s>']
 #  Replace UNKNOWN with a randomly selected word from unknown_words.
 def generate_sentence(prob_df, unknown_words):
-    return []
+    words = [word for word in prob_df.columns]
+    weights = [prob_df.iloc[0][i] for i in prob_df.columns]
+    sentence = ['<s>']
+
+    columns = prob_df.columns
+    while 1:
+        print(sentence[-1], columns, prob_df.loc[sentence[-1]])
+        sentence += random.choices(prob_df.columns, prob_df.loc[sentence[-1]])
+        if sentence[-1] == '</s>': break
+    return sentence
 
 
 # ToDo:
@@ -152,7 +161,7 @@ def build_model(bigram_counts, d=.75):
     for col in columns:
         for row_idx, row in enumerate(smooth_bigram_counts.index):
             smooth_bigram_counts.at[row, col] = \
-                laplace(smooth_bigram_counts.at[row, col], row_sums[row_idx],  len(columns))
+                laplace(smooth_bigram_counts.at[row, col], row_sums[row_idx], len(columns))
     return smooth_bigram_counts
 
 
--- test_Add1BigramModel.py
+++ test_Add1BigramModel.py
@@ -19,9 +19,15 @@ class Test(TestCase):
         print(add1.get_bigram_counts(training_data))
         self.assertTrue(True)
 
-    def test_get_build_model_sam_i_am(self):
+    def test_get_build_model_sam_i_am_output(self):
         training_data = add1.read_and_preprocess_corpus("Data/sam_i_am.txt")
         add1.build_model(add1.get_bigram_counts(training_data))
+        self.assertTrue(True)
+
+    def test_get_build_model_sam_i_am_output(self):
+        training_data = add1.read_and_preprocess_corpus("Data/sam_i_am.txt")
+        sentence = add1.generate_sentence(add1.build_model(add1.get_bigram_counts(training_data)), "cuck")
+        print(sentence)
         self.assertTrue(True)
 # class Test(TestCase):
 #     def test_build_model(self):
